ff6aeba8c6dfa4a151e9bb45c50ca166951c40bc
Flowery <45811382+Pink-Smile@users.noreply.github.com>
2021-01-03 19:27:17 +0800

    Revert "完成歌手图片上传"
    This reverts commit efe79d6016391415e0740d61568c2214654cc35c.

--- music-server/src/main/java/com/buct/music/enums/SexEnum.java
+++ music-server/src/main/java/com/buct/music/enums/SexEnum.java
@@ -1,7 +1,6 @@
 package com.buct.music.enums;
 
 /**
- * 性别枚举类
  * @author Zhang JunFeng
  * @date 2021/1/2 17:44
  */
@@ -9,7 +8,7 @@ public enum SexEnum {
     MALE(1),
     FEMALE(0);
 
-    private final Integer state;
+    private Integer state;
 
     SexEnum(Integer state) {
         this.state = state;
