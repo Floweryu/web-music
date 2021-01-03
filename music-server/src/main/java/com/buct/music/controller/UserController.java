825c0905f3ff4d81c632f827abaf8cc5c9196a31
carjeff <747938871@qq.com>
2021-01-03 22:43:14 +0800

    增加API
    添加增删改查API

--- music-server/src/main/java/com/buct/music/controller/UserController.java
+++ music-server/src/main/java/com/buct/music/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.buct.music.controller;
 
+import com.buct.music.controller.dto.UserReq;
 import com.buct.music.domin.Singer;
 import com.buct.music.domin.User;
 import com.buct.music.result.CodeMsg;
 import com.buct.music.result.Result;
 import com.buct.music.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author JeffCar
@@ -26,11 +26,11 @@ public class UserController {
     }
 
     /**
-     *
+     * 通过主键id查找用户
      * @param id
      * @return
      */
-    @GetMapping("/users")
+    @GetMapping("/user")
     public Result<User> getByPrimaryKey(@RequestParam Long id){
         if (id <= 0) {
             return Result.error(400, "id should > 0 !");
@@ -39,6 +39,105 @@ public class UserController {
             User user = userService.selectUserById(id);
             log.info("查询用户id: {}, 用户信息：{}", id, user);
             return Result.success(user);
+        } catch (Throwable throwable) {
+            log.error("There is something error: {}", throwable.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 添加用户
+     * @param req
+     * @return
+     */
+    @PostMapping("/user")
+    public Result<CodeMsg> addUser(@RequestBody UserReq req){
+        try {
+            boolean flag = userService.insert(req);
+            log.info("添加用户参数为: {}", req);
+            if (flag) {
+                return Result.success(CodeMsg.SUCCESS);
+            } else {
+                return Result.error(CodeMsg.FAILURE);
+            }
+        } catch (Throwable throwable) {
+            log.error("There is something error: {}", throwable.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 修改用户
+     * @param req
+     * @return
+     */
+    @PutMapping("/user")
+    public Result<CodeMsg> updateUser(@RequestBody UserReq req){
+        try {
+            boolean flag = userService.update(req);
+            log.info("修改用户参数为: {}", req);
+            if (flag) {
+                return Result.success(CodeMsg.SUCCESS);
+            } else {
+                return Result.error(CodeMsg.FAILURE);
+            }
+        } catch (Throwable throwable) {
+            log.error("There is something error: {}", throwable.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/user")
+    public Result<CodeMsg> deleteUser(@RequestParam Long id){
+        if (id <= 0) {
+            return Result.error(400, "id should > 0 !");
+        }
+        try {
+            boolean flag = userService.delete(id);
+            log.info("删除用户id: {}", id);
+            if (flag) {
+                return Result.success(CodeMsg.SUCCESS);
+            }else {
+                return Result.error(CodeMsg.FAILURE);
+            }
+        } catch (Throwable throwable) {
+            log.error("There is something error: {}", throwable.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 查找所有用户
+     * @return
+     */
+    @GetMapping("/users")
+    public Result<List<User>> selectAllUsers(){
+        try {
+            List<User> users = userService.selectAllUsers();
+            log.info("查询用户: {}", users);
+            return Result.success(users);
+        } catch (Throwable throwable) {
+            log.error("There is something error: {}", throwable.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 通过姓名查找用户
+     * @param username
+     * @return
+     */
+    @GetMapping("/users/username")
+    public Result<List<User>> selectUserByName(@RequestParam String username){
+        try {
+            List<User> users = userService.selectUserByName(username);
+            log.info("根据姓名查询用户: {}", users);
+            return Result.success(users);
         } catch (Throwable throwable) {
             log.error("There is something error: {}", throwable.getMessage());
             return Result.error(CodeMsg.SERVER_ERROR);
--- music-server/src/main/java/com/buct/music/service/impl/UserServiceImpl.java
+++ music-server/src/main/java/com/buct/music/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.buct.music.service.impl;
 
+import com.buct.music.controller.dto.UserReq;
 import com.buct.music.dao.UserMapper;
 import com.buct.music.domin.User;
 import com.buct.music.service.UserService;
+import com.buct.music.utils.UserServiceUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author JeffCar
@@ -21,5 +26,38 @@ public class UserServiceImpl implements UserService {
     @Override
     public User selectUserById(Long id) {
         return userMapper.selectUserById(id);
+    }
+
+    @Override
+    public boolean insert(UserReq userReq) {
+        User user = UserServiceUtil.setUser(userReq);
+        long time = System.currentTimeMillis();
+        user.setCreateTime(time);
+        user.setUpdateTime(time);
+        return userMapper.insert(user) > 0;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+
+        return userMapper.delete(id) > 0;
+    }
+
+    @Override
+    public boolean update(UserReq userReq) {
+        User user = UserServiceUtil.setUser(userReq);
+        long time = System.currentTimeMillis();
+        user.setUpdateTime(time);
+        return userMapper.update(user) > 0;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return userMapper.selectAllUsers();
+    }
+
+    @Override
+    public List<User> selectUserByName(String username) {
+        return userMapper.selectUserByName("%" + username + "%");
     }
 }
