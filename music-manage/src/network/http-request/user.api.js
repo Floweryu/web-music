3903da17fe777e17b5992f4fc2d166a4c2296211
carjeff <747938871@qq.com>
2021-01-07 21:54:34 +0800

    更改编辑操作，加入搜索按钮

--- music-manage/src/network/http-request/user.api.js
+++ music-manage/src/network/http-request/user.api.js
@@ -11,12 +11,17 @@ const user = {
     return axios.get(`${host.apiHost}/admin/users`)
   },
   //删除对应id用户
-  deletUser(params) {
+  deleteUser(params) {
+    // console.log(params);
     return axios.delete(`${host.apiHost}/admin/user`, params)
   },
   //更新用户
   updateUser(data) {
-    return axios.put(`${host.apiHost}/admin/user`, {}, data)
+    return axios.put(`${host.apiHost}/admin/user`, data)
+  },
+  //根据username查用户
+  getUserByName(params) {
+    return axios.get(`${host.apiHost}/admin/users/username`, params)
   }
 }
 
