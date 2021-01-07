3aa6bed9f39981789d31d580f4b1b9cfa1768440
carjeff <747938871@qq.com>
2021-01-08 02:19:20 +0800

    增加登录功能

--- music-manage/src/network/http-request/index.js
+++ music-manage/src/network/http-request/index.js
@@ -5,9 +5,11 @@
 import singer from './singer.api'
 import songs from './songs.api'
 import user from './user.api'
+import admin from './admin.api'
 
 export default {
   singer,
   songs,
-  user
+  user,
+  admin
 }
